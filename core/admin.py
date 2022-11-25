62fd340993f09b05ce8c7e5d302523faa7834624
Alan Felipe Aliske <alan.aliske@compagas.local>
2022-11-25 11:37:23 -0300

    update

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
-
 from django.contrib import admin 
 
+'''
 
 from core.models import CorePosts, CoreRespostas, CorePerguntas, CoreIdiomas
  
@@ -53,4 +53,3 @@ class IdiomasAdmin(admin.ModelAdmin):
     list_filter = ('idiomas_descri',)
 admin.site.register(Idiomas, IdiomasAdmin)
 
-'''
