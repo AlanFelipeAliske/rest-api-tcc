737383f997a7d43f51993f9bb4f8f695bd71e055
Alan Felipe Aliske <alan.aliske@compagas.local>
2022-11-28 15:38:45 -0300

    update

--- core/models.py
+++ core/models.py
@@ -38,3 +38,52 @@ class Respostas(models.Model):
     idioma = models.ForeignKey(Idiomas, models.DO_NOTHING, blank=True, null=True)
     pergunta = models.ForeignKey(Perguntas, models.DO_NOTHING, blank=True, null=True)
     
+
+
+class CoreCoreidiomas(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    idiomas_descri = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'core_coreidiomas'
+
+
+class CoreCoreperguntas(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    perguntas_descri = models.CharField(max_length=255)
+    idioma = models.ForeignKey(CoreCoreidiomas, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'core_coreperguntas'
+
+
+class CoreCoreposts(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    post_titulo = models.CharField(max_length=100)
+    post_descricao = models.CharField(max_length=255)
+    post_conteudo = models.TextField()
+    imagem = models.CharField(max_length=255)
+    idioma = models.ForeignKey(CoreCoreidiomas, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'core_coreposts'
+
+
+class CoreCorerespostas(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    pais_de_origem = models.CharField(max_length=255)
+    tempo_no_brasil = models.CharField(max_length=255)
+    esta_empregado = models.CharField(max_length=255)
+    dificuldade_imigrante = models.CharField(max_length=255)
+    idioma = models.ForeignKey(CoreCoreidiomas, models.DO_NOTHING, blank=True, null=True)
+    pergunta = models.ForeignKey(CoreCoreperguntas, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'core_corerespostas'
+
